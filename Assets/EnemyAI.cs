0f23f5547ee22d35b08721f07bd1869989c731f7
EdiL-Perez <158606465+EdiL-Perez@users.noreply.github.com>
2026-05-02 19:08:23 -0600

    Practica4_Edilberto_Perez
    ImplementacionPractica4

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -8,11 +8,17 @@ public class EnemyAI : MonoBehaviour
     public Transform Player;
     public float rango = 10f;
     private NavMeshAgent agente;
+    private Animator anim;
+
+    public AudioSource reproductorMusica; 
+    public AudioClip musicaExploracion; 
+    public AudioClip musicaEnemigo;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agente = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         
     }
 
@@ -23,14 +29,27 @@ public class EnemyAI : MonoBehaviour
         if(Player != null){
             float distancia = Vector3.Distance(transform.position,Player.position);
             if(distancia <= rango){
+                CambiarPista(musicaEnemigo);
+                anim.SetBool("Running",true);
                 agente.SetDestination(Player.position);
                 
             }
             else if (agente.hasPath)
             {
+                CambiarPista(musicaExploracion);
+                anim.SetBool("Running",false);
                 agente.ResetPath();
             }
         }
         
+    }
+
+    void CambiarPista(AudioClip nuevaPista)
+    {
+        if (reproductorMusica.clip == nuevaPista) return;
+        
+        reproductorMusica.Stop(); 
+        reproductorMusica.clip = nuevaPista; 
+        reproductorMusica.Play(); 
     }
 }
